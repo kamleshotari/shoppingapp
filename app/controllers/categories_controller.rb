2d969481a416aea006b715aa9e787a91d769e41f
Siddhi Prabha <prabhasiddhi9@gmail.com>
2015-12-17 22:34:32 -0800

    Product Complete

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -31,6 +31,15 @@ class CategoriesController < ApplicationController
   end
 
   def update
+    respond_to do |format|
+      if @category.update(category_params)
+        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
+        format.json { render :show, status: :ok, location: @category }
+      else
+        format.html { render :edit }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -31,6 +31,26 @@ class ProductsController < ApplicationController
   def edit
     @product = Product.find(params[:id])
   end
+  def update
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.json { render :show, status: :ok, location: @product }
+      else
+        format.html { render :edit }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
  private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
