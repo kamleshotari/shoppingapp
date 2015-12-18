91787d9229bb9c14a0b27c8f104386ed82fb3221
Siddhi Prabha <prabhasiddhi9@gmail.com>
2015-12-17 22:18:58 -0800

    Product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,9 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
   def index
+    @products = Product.all
+    @products = Product.all
   end
 
   def new
@@ -7,13 +11,35 @@ class ProductsController < ApplicationController
   end
   
   def create
+    @product = Product.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { render :new }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
+ private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
 
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def product_params
+      params.require(:product).permit(:title, :description, :price, :category_id, :uniq_code, :code, :color)
+    end
   
 end
