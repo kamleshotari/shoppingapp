8fc2253c1a49e04a84ac9e429fb9ed69faf75f35
heenag <heenag@quinnox.com>
2015-12-18 02:07:53 -0800

    user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get '/sign_up' => 'devise/registrations#new'
+end
   resources :products
   resources :categories
 
