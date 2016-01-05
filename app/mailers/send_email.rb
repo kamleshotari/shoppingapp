d67e7aa948044006edf2d5b595e0c9ad18393843
Kamlesh <kamleshotari.com>
2016-01-05 11:56:37 +0530

    email

--- app/mailers/send_email.rb
+++ app/mailers/send_email.rb
@@ -2,7 +2,7 @@ class SendEmail < ActionMailer::Base
 	
   def send_email(user)
     @user = user
-    mail(from: 'prabhasiddhi9@gmail.com', to: @user.email, subject: 'Confirmation Email')
+    mail(from: 'vickyrocks86@gmail.com', to: @user.email, subject: 'Confirmation Email')
   end
   
 end
