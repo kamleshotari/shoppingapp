e495525b5bec8a6e67ed2111e18899a2b931b46b
Siddhi Prabha <prabhasiddhi9@gmail.com>
2016-01-04 23:06:04 -0800

    edit button removed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
-
+before_action :set_user, only: [:destroy]
   def index
   	@users = User.all
     @users = User.page(params[:page]).per(5)
   end
 
-  
+  def show
+  	@user = User.find(params[:id])
+  end
 
+  def destroy
+  	@user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+  private
+    
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
