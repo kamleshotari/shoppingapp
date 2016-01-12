aa2a24f9fd5ebf4358569d1e796472fd1fafac4f
Siddhi Prabha <prabhasiddhi9@gmail.com>
2016-01-12 02:43:26 -0800

    Import page modified

--- app/controllers/product_import_controller.rb
+++ app/controllers/product_import_controller.rb
@@ -8,7 +8,7 @@ class ProductImportController < ApplicationController
   	@import_response = Product.import(params[:file])
     respond_to do |format|
     	#format.js
-    	format.html{redirect_to URI.encode("/product_import/new?s=#{@import_response.first.join(', ')}&f=#{@import_response.last.join(', ')}"), :notice => "Products imported successfully"}
+    	format.html{redirect_to URI.encode("/product_import/new?s=#{@import_response.first.join(', ')}&f=#{@import_response.last.join(', ')}")}
     end
   end
 end
